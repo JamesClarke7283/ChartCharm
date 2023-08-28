dcb8e1a22dce237ecd3fb6c2ae36b5dd0e761cc6
James Clarke <jc1222@canterbury.ac.uk>
2023-08-28 08:46:29 +0100

    Fixed more clippy warnings

--- src-ui/src/main.rs
+++ src-ui/src/main.rs
@@ -6,11 +6,11 @@
 )]
 #![allow(clippy::module_name_repetitions)]
 #![deny(warnings)]
-use leptos::*;
-use chartcharm_server::*;
+use leptos::{mount_to_body, view, warn};
+use chartcharm_server::App;
 
 pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx, <App/> })
+    mount_to_body(|cx| view! { cx, <App/> });
 }
