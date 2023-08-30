9c1aef24fe2fb2f7fc2fbe300b65862ed0d2faff
James David Clarke <james@jamesdavidclarke.com>
2023-08-30 11:09:46 +0100

    Fixed database not compiling for wasm issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,18 @@
     windows_subsystem = "windows"
 )]
 
+use chartcharm_database::init_db;
 use tauri::Builder;
 
-fn main() {
+#[async_std::main]
+async fn main() {
     Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    match init_db().await {
+        true => {}
+        false => {
+            panic!("Failed to connect to database");
+        }
+    };
 }
--- src-ui/src/main.rs
+++ src-ui/src/main.rs
@@ -1,17 +1,8 @@
-use chartcharm_database::init_db;
 use chartcharm_server::App;
 use leptos::{mount_to_body, view};
 
-#[async_std::main]
-pub async fn main() {
+pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     mount_to_body(|cx| view! { cx, <App/> });
-
-    match init_db().await {
-        true => {}
-        false => {
-            panic!("Failed to connect to database");
-        }
-    }
 }
